90c24179b0e195c58df653a2b5e1dc59df929618
Tomi Karikj <karikjtomi@gmail.com>
2019-11-18 20:10:43 +0100

    Cached and loaded the resources

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const staticCacheName = 'site-static';
+const staticAssets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/cook.svg',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://kit.fontawesome.com/63d74ff3d7.js'
+];
+
 // Service worker installation event
 self.addEventListener('install', e => {
-  console.log('The service worker has been installed');
+  e.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      cache.addAll(staticAssets);
+    })
+  );
 });
 
 // Service worker activation event
@@ -10,5 +28,9 @@ self.addEventListener('activate', e => {
 
 // Service worker fetch event
 self.addEventListener('fetch', e => {
-  // console.log('Fetch event: ', e);
+  e.respondWith(
+    caches.match(e.request).then(cacheRes => {
+      return cacheRes || fetch(e.request);
+    })
+  );
 });
